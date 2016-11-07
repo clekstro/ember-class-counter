acb921b9092e5b09925827d5d46afd72c0e73f4b
Curtis Ekstrom <code@fauxton.io>
2016-11-07 21:09:38 +0100

    Remove unnecessary use of included hook
    Just a vestige of previous experimentations

--- index.js
+++ index.js
@@ -4,10 +4,6 @@
 module.exports = {
   name: 'ember-class-counter',
 
-  included: function (app) {
-    this.app = app;
-  },
-
   setupPreprocessorRegistry: function(type, registry) {
     var ClassCounter = require('./class-counter');
 
